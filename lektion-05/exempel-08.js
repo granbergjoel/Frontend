a9da0b28709808f58fe3a48cddbb557745afd1a6
Joel Granberg <granberg.joel@gmail.com>
2020-12-11 10:56:12 +0100

    vg-delen

--- lektion-05/exempel-08.js
+++ lektion-05/exempel-08.js
@@ -8,3 +8,29 @@
         console.log(myName.toUpperCase());
 
 
+        //Objektet Math
+
+        console.log(Math.PI);
+        console.log(Math.round(2.1)); //Avrundar ner
+        console.log(Math.round(2.5)); // avrundar upp
+        console.log(Math.pow(2,5));
+        console.log(Math.pow(3,5));
+        console.log(Math.sqrt(16));
+        console.log(Math.min(5,3));
+        console.log(Math.max(5,3));
+
+        console.log(Math.random());
+        console.log(Math.random()*10+1); //Slumptal mellan 1 och 10
+        console.log(Math.round(Math.random()*10 +1)); //Avrundar slumptal mellan 1 och 10
+
+
+
+        //Objektet Date
+        let datum = new Date();
+        console.log(datum);
+        console.log(Date());
+        console.log(datum.getFullYear());
+        console.log(datum.getMonth()); // får 11? det är ju december? 
+        //det är för att månaderna går fråån 0-11 i JS. 0=januari, 11=december
+        console.log(datum.getDate());
+        console.log(datum.getDay());
---
+++ lektion-05/exempel-09.js
@@ -0,0 +1,8 @@
+let pi = 3.14159;
+// utan "" tecken blir tal auto till numbers
+
+console.log(pi);
+console.log(pi.toFixed(2)); // med två decimaler
+console.log(pi.toFixed(3)); //nu avrundas pi uppåt
+console.log(pi.toPrecision(3)); //visar tre tal totalt
+console.log(pi.toPrecision(4)); //visar 4 tal och avrundar
