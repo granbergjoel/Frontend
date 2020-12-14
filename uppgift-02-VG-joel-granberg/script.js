0fd317344f01151d365d5a1020ff6e7af0bef578
Joel Granberg <granberg.joel@gmail.com>
2020-12-14 11:45:47 +0100

    vg-delen, klurigt

--- uppgift-02-VG-joel-granberg/script.js
+++ uppgift-02-VG-joel-granberg/script.js
@@ -1,6 +1,51 @@
-let x;
-let y;
-let z;
-let operator;
+let x=0;
+let y=0;
+let input="123+456";
+let operator="";
+let result=0;
+let index=0;
+let boolean, loopiloop = true;
+let lengthOfIndex=input.length;
+
+function calculate(){
+ 
+    index = input.indexOf("+");
+    if (index > 0 && index < lengthOfIndex) {
+      
+    }
+    index = input.indexOf("-");
+    if (index > 0 && index < lengthOfIndex) {
+       
+    }
+    index = input.indexOf("/");
+    if (index > 0 && index < lengtOfIndex) {
+       
+    }
+    index = input.indexOf("*");
+    if (index > 0 && index < lengtOfIndex) {
+     
+    }
+   
+        x= parseFloat(input.substring(0,index));
+        y=parseFloat(input.substring(index+1));
+      
+
+    switch (operator) {
+        case '+':
+            System.out.println(x + ""+operator + y + "=" + (x + y));
+            break;
+        case '*':
+            System.out.println(x + "" + operator + y + " = " + (x * y));
+            break;
+        case '/':
+            System.out.println(x + "" + operator + y + "=" + (x / y));
+            break;
+        case '-':
+            System.out.println(x + "" + operator + y + "=" + (x - y));
+            break;
+    }
 
 
+
+    
+}
