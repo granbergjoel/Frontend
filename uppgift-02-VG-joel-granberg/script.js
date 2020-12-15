392fd6bef50802b2d0a28efb1c98f2a9450a883f
Joel Granberg <granberg.joel@gmail.com>
2020-12-15 11:53:13 +0100

    ...

--- uppgift-02-VG-joel-granberg/script.js
+++ uppgift-02-VG-joel-granberg/script.js
@@ -1,48 +1,56 @@
 let x=0;
 let y=0;
-let input="123+456";
+let input="10+5";
 let operator="";
 let result=0;
 let index=0;
 let boolean, loopiloop = true;
 let lengthOfIndex=input.length;
 
-
-
-function calculate(){
- 
-    index = input.indexOf("+");
-    if (index > 0 && index < lengthOfIndex) {
+  while(loopiloop){
+    
+      index = input.indexOf("-");
+      if (index > 0 && index < lengthOfIndex) {
+        operator=input.charAt(index);
+         break;
+      }
+      console.log(index)
+      index = input.indexOf("/");
+      if (index > 0 && index < lengtOfIndex) {
+        operator=input.charAt(index);
+        break;
+      }
+      console.log(index)
+      index = input.indexOf("*");
+      if (index > 0 && index < lengtOfIndex) {
+        operator=input.charAt(index);
+       break;
+        }
+        console.log(index)
+        index = input.indexOf("+");
+      if (index > 0 && index < lengthOfIndex) {
+        operator=input.charAt(index);
+       break;
+      }
       
-    }
-    index = input.indexOf("-");
-    if (index > 0 && index < lengthOfIndex) {
-       
-    }
-    index = input.indexOf("/");
-    if (index > 0 && index < lengtOfIndex) {
-       
-    }
-    index = input.indexOf("*");
-    if (index > 0 && index < lengtOfIndex) {
-     
-    }
-   
+        }
+        console.log(index)
         x= parseFloat(input.substring(0,index));
         y=parseFloat(input.substring(index+1));
-      
+      console.log(x, y, operator)
 
     switch (operator) {
         case '+':
-            System.out.println(x + ""+operator + y + "=" + (x + y));
+        console.log(x + ""+operator + y + "=" + (x + y));
             break;
         case '*':
-            System.out.println(x + "" + operator + y + " = " + (x * y));
+        console.log(x + "" + operator + y + " = " + (x * y));
             break;
         case '/':
-            System.out.println(x + "" + operator + y + "=" + (x / y));
+        console.log(x + "" + operator + y + "=" + (x / y));
             break;
         case '-':
-            System.out.println(x + "" + operator + y + "=" + (x - y));
+        console.log(x + "" + operator + y + "=" + (x - y));
             break;
-    }}
+    }
+ 
