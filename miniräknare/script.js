fc3a865a4dde963b7a4d442bf461462e46b7a185
Joel Granberg <granberg.joel@gmail.com>
2020-12-16 11:29:51 +0100

    lektion 06

--- miniräknare/script.js
+++ miniräknare/script.js
@@ -1,14 +1,21 @@
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator__keys")
+const display = document.querySelector('.calculator__display')
 var e="";
 keys.addEventListener("click", e => {
  if (e.target.matches("button")) {
  
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
 
-if (!action) {
-    console.log('number key!')
+  if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent;
+    }else {
+      display.textContent = displayedNum + keyContent
+    }
   }
   if (
     action === 'add' ||
@@ -16,11 +23,17 @@ if (!action) {
     action === 'multiply' ||
     action === 'divide'
   ) {
+    key.classList.add('is-depressed')
+    calculator.dataset.previousKeyType = 'operator'
+    display.textContent = displayedNum + keyContent
+  }
+  
+  {
     console.log('operator key!')
   }
 
   if (action === 'decimal') {
-    console.log('decimal key!')
+    display.textContent = displayedNum + '.'
   }
   
   if (action === 'clear') {
@@ -31,7 +44,9 @@ if (!action) {
     console.log('equal key!')
   }
 
- }
+  Array.from(key.parentNode.children)
+  .forEach(k => k.classList.remove('is-depressed'))
+}
 })
 
 
